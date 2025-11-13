e6f2f5a270ac3a7b44f5ba62f21d8a741905b9c5
CHarris-VR <charrisagent@gmail.com>
2025-11-13 03:23:12 -0500

    updated the varibales within definitions and ran code to test it

--- charris-firstpy.py
+++ charris-firstpy.py
@@ -1,3 +1,14 @@
 # This is my first Python script!
+
 print('Hello Analyst!')
 print('Welcome to Pyhton for Cybersecurity!')
+
+# Week 1 - Python Basics
+# Author: Curtis Harris
+# Purpose: Print some basic info about the system
+
+analyst_name = "Jordan"
+organization = "EvilCorp Cyber Defense"
+
+print("Analyst:", analyst_name)
+print("Ogranization:", organization)
