c20859f8414ac964bcac9e67c91062b84364d029
CHarris-VR <charrisagent@gmail.com>
2025-11-13 03:33:56 -0500

    updated code for inputs for username, organization and print within the tool.

--- charris-firstpy.py
+++ charris-firstpy.py
@@ -7,8 +7,8 @@ print('Welcome to Pyhton for Cybersecurity!')
 # Author: Curtis Harris
 # Purpose: Print some basic info about the system
 
-analyst_name = "Jordan"
-organization = "EvilCorp Cyber Defense"
+analyst_name = input("enter your name: ")
+organization = input("Enter this system's role (e.g., web server, workstation): ")
 
 print("Analyst:", analyst_name)
 print("Ogranization:", organization)
@@ -19,3 +19,12 @@ print ("n--- System Information ---")
 print ("Hostname:", platform.node())
 print ("Operating System:", platform.system(), platform.release())
 print ("Current User:", os.getlogin())
+
+# Added code to display the information of the entered Analyst name and role
+
+print(f"\nSecurity Report for {analyst_name}")
+print("-------------------------------")
+print("System Role:", system_role)
+print("Hostname:", platform.node())
+print("Operating System:", platform.system(), platform.release())
+print("Current User:", os.getlogin())
