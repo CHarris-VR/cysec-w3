9e1f3e0ec6e55c8c15269f22a2d47edfbe004c1f
CHarris-VR <charrisagent@gmail.com>
2025-11-13 04:20:46 -0500

    Added reflect.txt and added things to generate report generated daye and time

--- system_profile.py
+++ system_profile.py
@@ -4,16 +4,19 @@ analyst_name = input("enter your name: ")
 dept_role = input("Enter which Department you are from(e.g., Security, Management): ")
 
 
-# Print the Security Analyst role, department.
+# Print the Security Analyst role, department and date time of creation
 print("-------------------------------")
 print(f"  Security Report for {analyst_name}")
 print(f"  From the {dept_role} Department. ")
 print(" ")
+print("Report Generated:", datetime.now())
 print("-------------------------------")
 
 # Importing tools to call functions from windows system
 import platform
 import os
+from datetime import datetime
+
 
 # Print out of the system info and current user.
 print("Department Role:", dept_role)
