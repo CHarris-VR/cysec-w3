a32aadf31abc5368fced5e046de563338c453588
CHarris-VR <charrisagent@gmail.com>
2025-11-13 04:11:48 -0500

    cleaned up the code and added a message to signal the report was generated successfully

--- system_profile.py
+++ system_profile.py
@@ -4,10 +4,23 @@ analyst_name = input("enter your name: ")
 dept_role = input("Enter which Department you are from(e.g., Security, Management): ")
 
 
-# Printing the Security Analyst role, department, and system information.
-print(f"  Security Report for {dept_role}")
+# Print the Security Analyst role, department.
 print("-------------------------------")
-print("System Role:", system_role)
+print(f"  Security Report for {analyst_name}")
+print(f"  From the {dept_role} Department. ")
+print(" ")
+print("-------------------------------")
+
+# Importing tools to call functions from windows system
+import platform
+import os
+
+# Print out of the system info and current user.
+print("Department Role:", dept_role)
 print("Hostname:", platform.node())
 print("Operating System:", platform.system(), platform.release())
 print("Current User:", os.getlogin())
+print("-------------------------------")
+print(" ")
+print("Report generated successfully!")
+print("-------------------------------")
