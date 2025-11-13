ebee8916536fa196b94eb3c6fad439c5474c807e
CHarris-VR <charrisagent@gmail.com>
2025-11-13 04:30:55 -0500

    made several changes to make the code look better and update the Reflect.txt for the assignment

--- system_profile.py
+++ system_profile.py
@@ -3,27 +3,37 @@
 analyst_name = input("enter your name: ")
 dept_role = input("Enter which Department you are from(e.g., Security, Management): ")
 
+# Making a COOL TITLE so it can be EXTRA EPIC!!! :D
+print("="*40)
+print(" EvilCorp Cyber Defense - System Profile ")
+print("="*40)
 
-# Print the Security Analyst role, department and date time of creation
-print("-------------------------------")
+
+# Print the Security Analyst role, department
+
 print(f"  Security Report for {analyst_name}")
 print(f"  From the {dept_role} Department. ")
-print(" ")
-print("Report Generated:", datetime.now())
-print("-------------------------------")
+
 
 # Importing tools to call functions from windows system
+
 import platform
 import os
 from datetime import datetime
 
+# Print when the report was generated
+print("-"*40)
+print("Report Generated:", datetime.now())
+print("-"*40)
+
 
 # Print out of the system info and current user.
+
 print("Department Role:", dept_role)
 print("Hostname:", platform.node())
 print("Operating System:", platform.system(), platform.release())
 print("Current User:", os.getlogin())
-print("-------------------------------")
+print("-"*40)
 print(" ")
 print("Report generated successfully!")
-print("-------------------------------")
+print("-"*40)
